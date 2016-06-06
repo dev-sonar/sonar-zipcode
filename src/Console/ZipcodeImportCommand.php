e869388d5c85ff7d6307e2acf3b4445471578f4f
webadmin <webadmin@localhost.localdomain>
2016-06-05 18:57:12 -0700

    fire調整

--- src/Console/ZipcodeImportCommand.php
+++ src/Console/ZipcodeImportCommand.php
@@ -55,12 +55,11 @@ class ZipcodeImportCommand extends Command
 
         $data = $this->curl->to(self::URL)->get();
         $this->file->put(storage_path($path . '/ken_all.zip'), $data);
-        $zip_file = str_replace(base_path() . '/', '', storage_path( $path . '/ken_all.zip'));
-        $extract_path = str_replace(base_path() . '/', '', storage_path($path));
 
-        $zip = $this->zipper->make($zip_file);
-        $zip->extractTo($extract_path . "/");
-        $zip->close();
+        $this->extractZip(
+            str_replace(base_path() . '/', '', storage_path( $path . '/ken_all.zip')),
+            str_replace(base_path() . '/', '', storage_path($path))
+        );
 
         $this->file->put(
             storage_path($path . '/ken_all.csv.utf8'),
@@ -73,17 +72,33 @@ class ZipcodeImportCommand extends Command
         $config_file = __DIR__ . '/zipcodes.yml';
         $this->import->setConfig($config_file);
 
+        $this->checkAddPrefecture();
+        $this->checkAddCity();
+
+        $this->import->csvRead(storage_path($path . '/ken_all.csv.new'));
+    }
+
+    public function extractZip($zip_file,$extract_path)
+    {
+        $zip = $this->zipper->make($zip_file);
+        $zip->extractTo($extract_path . "/");
+        $zip->close();
+    }
+
+    protected function checkAddPrefecture()
+    {
         if ( $this->option('add-prefecture') ) {
             $prefecture_config_file = __DIR__ . '/prefectures.yml';
             $this->import->setPrefectureConfig($prefecture_config_file);
             $this->import->setIsPrefecture(true);
         }
+    }
+    protected function checkAddCity()
+    {
         if ( $this->option('add-city') ) {
             $city_config_file = __DIR__ . '/cities.yml';
             $this->import->setCityConfig($city_config_file);
             $this->import->setIsCity(true);
         }
-
-        $this->import->csvRead(storage_path($path . '/ken_all.csv.new'));
     }
 }
